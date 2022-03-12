6bd9250538ee1205f019589ce7122a4c7167b1e8
Alamin-git <alamin2.mas@gmail.com>
2022-03-12 23:35:21 +0600

    folder organige & slider add

---
+++ outhers/arrowFunction.js
@@ -0,0 +1,20 @@
+// arrow function
+
+// single line arrow fungtion
+/* 
+single line er jonno return kora lage na.
+=> er porer tuku auto return kore dei. 
+
+*/
+const getNumber = () => 44;
+const getNumber2 = num => num * 3;
+const addNum = (x, y, z) => x + y + z;
+
+// multiline arrow function
+/* 
+multiline function a return na korle se o return kore na .
+*/
+const add = (num1, num2) => {
+    const sum = num1 + num2;
+    return sum;
+}
